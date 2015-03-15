1831649f326b12ae2003ee8759b786f610672e48
MikeParadis <Mystikmagik@users.noreply.github.com>
2015-03-15 02:53:14 -0500

    Update index.js
    Added menus

--- js/index.js
+++ js/index.js
@@ -1,11 +1,61 @@
-
 var courses;
 
+$(document).ready(function (){
+    $('.filter-menu-toggle').on('click', function(event){
+        event.preventDefault();
+        // create menu variables
+        var filterMenu = $('.filter-menu');
+        var filterMenuWidth = $('.filter-menu').width();
+        
+        // toggle open class
+        filterMenu.toggleClass("open");
+        
+        // slide menu
+        if(filterMenu.hasClass("open")){
+            filterMenu.animate({
+                left: "0px"
+            });
+        }else{
+            filterMenu.animate({
+                left: -filterMenuWidth
+            }, 200);
+        }
+    });
+
+    $("#clear-filters").click(function(){
+        $(".filterbox").each(function(){
+            this.checked = false;
+        });
+    });
+
+
+    $('.worksheet-menu-toggle').on('click', function(event){
+        event.preventDefault();
+        // create menu variables
+        var worksheetMenu = $('.worksheet-menu');
+        var worksheetMenuWidth = $('.worksheet-menu').width();
+        
+        // toggle open class
+        worksheetMenu.toggleClass("open");
+        
+        // slide menu
+        if(worksheetMenu.hasClass("open")){
+            worksheetMenu.animate({
+                right: "0px"
+            });
+        }else{
+            worksheetMenu.animate({
+                right: -worksheetMenuWidth
+            }, 600);
+        }
+    });
+});
+
 $(function() {
-	courses = courseData;
+    courses = courseData;
 });
 
 
 function getData() {
-	console.log(courses);
+    console.log(courses);
 };
