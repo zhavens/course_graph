d7b1a84b0da3cd4e64a3691be6a34f3dac4824b0
MaddieLvrg <m_lavergne_92@msn.com>
2015-03-15 17:59:04 -0500

    Gross graph
    We're gonna use Jit's spacetree but this will do for now.

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+
+      google.load("visualization", "1", {packages:["orgchart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('string', 'Manager');
+        data.addColumn('string', 'ToolTip');
+
+        data.addRows([
+          [{v:'COMP1010'},'', 'Introduction to Computer Science 1'],
+          [{v:'COMP1020'},'', 'Introduction to Computer Science 2'],
+          ['COMP2140', 'COMP1020', 'Data Structures and Algorithms'],
+          ['COMP3010', 'COMP2140', 'Distributed Computing'],
+          ['COMP3020', 'COMP2140', 'Human-Computer Interaction 1']
+        ]);
+
+        var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+        chart.draw(data, {allowHtml:true});
+      }
