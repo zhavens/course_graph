4e573e9ada8d17414dfb93657b687df46d546e2d
MaddieLvrg <m_lavergne_92@msn.com>
2015-03-15 21:10:04 -0500

    Show / Hide calendar

--- js/index.js
+++ js/index.js
@@ -88,3 +88,11 @@ function getData(name) {
 
     return -1;
 };
+
+function toggleDisplay(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
